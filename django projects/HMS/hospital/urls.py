8d98beda3dae631ee2a9710ef6d4f8d994ba555b
Hemil Kaklotar <hemilkaklotar@gmail.com>
2021-09-18 16:53:50 +0530

    Make Migration In HMS and Some Changes

---
+++ django projects/HMS/hospital/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from hospital.views import *
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('', about, name='about'),
+
+]
