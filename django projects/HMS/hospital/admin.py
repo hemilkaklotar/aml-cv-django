82d5bf6e7eecdaa06d2762e245abe683593a4ade
Hemil Kaklotar <hemilkaklotar@gmail.com>
2021-09-19 21:14:13 +0530

    Footer added and Database created

--- django projects/HMS/hospital/admin.py
+++ django projects/HMS/hospital/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from hospital.models import Doctor,Patient,Appointment
 # Register your models here.
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
--- django projects/HMS/hospital/models.py
+++ django projects/HMS/hospital/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    mobile = models.IntegerField()
+    special = models.CharField(max_length=50)
+
+class Patient(models.Model):
+    name= models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+    mobile = models.IntegerField(null=True)
+    address = models.TextField()
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
