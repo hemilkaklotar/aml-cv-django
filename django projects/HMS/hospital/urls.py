6e3888baf279d5ae1a879f89d2d0c3a4bd837b04
Hemil Kaklotar <hemilkaklotar@gmail.com>
2021-09-20 15:45:39 +0530

    Login Logout..

--- django projects/HMS/hospital/urls.py
+++ django projects/HMS/hospital/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from hospital.views import *
+from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
     path('about-us/', about, name='about'),
-    path('contact-us/', contact, name='contact')
-
+    path('contact-us/', contact, name='contact'),
+    path('admin_login/', login, name='login'),
+    path('logout/', logout_admin, name='logout_admin'),
 ]
--- django projects/HMS/hospital/views.py
+++ django projects/HMS/hospital/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 def home(request):
     return render(request, 'home.htm')
@@ -10,3 +11,32 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.htm')
+
+def Index(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    return render(request, 'index.htm')
+
+def login(request):
+    error = ""
+    if request.method == "POST":
+        username_a = request.POST['uname']
+        password_a = request.POST['pwd']
+        user = authenticate(username = username_a,password = password_a)
+        try :
+            if user.is_staff:
+                login(request,user)
+                error = 'No'
+            else:
+                error = "Yes"
+        except:
+            error = "Yes"
+    d = {'error':error}
+    return render(request,'login.htm', d )
+
+def logout_admin(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    logout(request)
+    return redirect('login')
+
