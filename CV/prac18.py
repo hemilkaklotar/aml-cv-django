ac30be844c72b6e36e585b1c7fb8e019fac770db
Hemil Kaklotar <hemilkaklotar@gmail.com>
2022-01-06 12:31:37 +0530

    Changed

--- CV/prac18.py
+++ CV/prac18.py
@@ -2,7 +2,6 @@
 #find contours
 import cv2
 
-
 image=cv2.imread('Images/a3.jpg')
 
 #converting RGB image to binary
@@ -14,7 +13,6 @@ cv2.imshow('thresh',thresh)
 contours,hierarchy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 with_contours=cv2.drawContours(image,contours,-1,(0,255,0),3)
 cv2.imshow('contours',with_contours)
-
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
